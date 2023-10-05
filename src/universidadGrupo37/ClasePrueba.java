b6cf6e1f41a851c6fa938dc30ea40c909e29254a
Santi-Villagran <141844073+Santi-Villagran@users.noreply.github.com>
2023-10-05 11:09:20 -0300

    hola

---
+++ src/universidadGrupo37/ClasePrueba.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadGrupo37;
+
+/**
+ *
+ * @author santi
+ */
+public class ClasePrueba {
+    private String hola="hola";
+    
+}
